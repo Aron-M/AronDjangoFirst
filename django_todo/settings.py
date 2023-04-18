c05826b2fe567b02d02d366e79a614f0f89ffea8
Aron-M <aron.moelich88@gmail.com>
2023-04-18 05:52:31 +0000

    Set up environment variables and automatic deployments2

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8gz*c*z1t_ps_kpwijyd=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME")]
 
 
 # Application definition
